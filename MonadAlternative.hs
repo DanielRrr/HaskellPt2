6b08e7ab13242cac6e6c0447844cc62bf89da333
Daniel <chelsea2005joecole@icloud.com>
2017-04-22 02:03:13 +0300

    added

--- MonadAlternative.hs
+++ MonadAlternative.hs
@@ -18,3 +18,19 @@ xs (<**>) fs = (pure &) <*> xs <*> fs
 
 (<***>) :: Applicative f => f a -> f (a -> b) -> f b
 (<***>) = flip (<*>)
+
+newtype PrsE a = PrsE { runPrsE :: String -> Either String (a, String) }
+
+instance Monad PrsE where
+  p >>= f = PrsE fun where
+    fun s = do
+      (d, s') <- (runPrsE p s)
+      runPrsE (f d) s'
+
+
+data OddC a = Un a | Bi a a (OddC a) deriving (Eq,Show)
+
+concat3OC :: OddC a -> OddC a -> OddC a -> OddC a
+concat3OC (Un x) (Un y) z = Bi x y z
+concat3OC (Un l) (Bi r1 r2 rt) c = Bi l r1 $ concat3OC (Un r2) rt c
+concat3OC (Bi l1 l2 lt) r c = Bi l1 l2 $ concat3OC lt r c
