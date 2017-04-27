a940ae4326a31594d96283425b30c81592629223
Daniel <chelsea2005joecole@icloud.com>
2017-04-27 15:04:50 +0300

    added

--- MonadExcept.hs
+++ MonadExcept.hs
@@ -1,7 +1,10 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Demo where
 
 import Control.Monad (liftM, ap, MonadPlus(mzero, mplus), guard, msum)
 import Control.Applicative (Alternative(empty, (<|>)))
+import Data.Bifunctor
+import Text.Read
 
 newtype Except e a = Except { runExcept :: Either e a} deriving Show
 
@@ -71,3 +74,48 @@ example1 x y = action `catchE` handler where
     return $ show q
   handler (ErrZero s) = return s
   handler ErrOther = return "NONNEGATIVE GUARD"
+
+newtype SimpleError = Simple { getSimple :: String }  deriving (Eq, Show)
+
+data ListIndexError = ErrIndexTooLarge Int | ErrNegativeIndex
+
+instance Monoid SimpleError where
+   mempty = Simple ""
+   (Simple l) `mappend` (Simple r) = Simple (l ++ r)
+
+lie2se :: ListIndexError -> SimpleError
+lie2se (ErrIndexTooLarge n) = Simple ("[index (" ++ (show n) ++ ") is too large]")
+lie2se ErrNegativeIndex = Simple "[negative index]"
+
+withExcept :: (e -> e') -> Except e a -> Except e' a
+withExcept f (Except e) = (Except (first f e))
+
+newtype Validate e a = Validate { getValidate :: Either [e] a } deriving (Show, Eq, Functor, Applicative, Monad)
+
+data SumError = SumError Int ReadError
+  deriving Show
+
+data ReadError = EmptyInput | NoParse String
+    deriving Show
+
+tryRead :: Read a => String -> Except ReadError a
+tryRead "" = throwE EmptyInput
+tryRead str = case readMaybe str of
+  Nothing -> throwE $ NoParse str
+  Just result -> return result
+
+trySum :: [String] -> Except SumError Integer
+trySum = except . snd . foldl (\ (n, sum) x -> (n + 1, (+) <$> sum <*> tryReadNth n x)) (1, Right 0)
+    where tryReadNth n x = runExcept $ withExcept (SumError n) (tryRead x)
+
+collectE :: Except e a -> Validate e a
+collectE er = case (runExcept er) of
+  (Left l) -> Validate (Left [l])
+  Right r -> Validate (Right r)
+
+instance Monoid a => Monoid (Validate e a) where
+  mempty = Validate (Right mempty)
+  Validate (Left x) `mappend` Validate (Left y) = Validate (Left (x `mappend` y))
+  Validate (Right _) `mappend`  something = something
+  something `mappend` Validate (Right _) = something
+  Validate (Right x) `mappend` Validate (Right y) = Validate (Right (x `mappend` y))
